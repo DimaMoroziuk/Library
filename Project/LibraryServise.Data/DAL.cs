d253dd3e83ce2e08dba545173a21970035bf6604
stein4444 <eysmont.vitya@gmail.com>
2021-03-23 12:28:26 +0200

    created db genre book author

---
+++ Project/LibraryServise.Data/DAL.cs
@@ -0,0 +1,47 @@
+﻿using LibraryServise.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryServise.Data
+{
+    public class DAL
+    {
+        private MyDbContext ctx;
+
+        public DAL()
+        {
+            ctx = new MyDbContext();
+        }
+
+        public IQueryable<Book> GetBooks()
+        {
+            return ctx.Books;
+        }
+        public IQueryable<Author> GetAuthors()
+        {
+            return ctx.Authors;
+        }
+        public IQueryable<Genre> GetGenres()
+        {
+            return ctx.Genres;
+        }
+        public void AddAuhor(Author author)
+        {
+            ctx.Authors.Add(author);
+            ctx.SaveChangesAsync();
+        }
+        public void AddBook(Book book)
+        {
+            ctx.Books.Add(book);
+            ctx.SaveChangesAsync();
+        }
+        public void AddGenre(Genre genre)
+        {
+            ctx.Genres.Add(genre);
+            ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ Project/LibraryServise.Data/EF/MyDbContext.cs
@@ -0,0 +1,21 @@
+using LibraryServise.Data.Entities;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LibraryServise.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+            : base("name=DbShopBooks")
+        {
+        }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+
+    }
+
+    
+}
